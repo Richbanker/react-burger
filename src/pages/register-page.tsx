7cdcbca978bc28e20012ca3216d27ed7740b04dd
Richbanker <123333089+Richbanker@users.noreply.github.com>
2026-09-26 07:56:18 +0300

    fix: исправить форматирование сообщения регистрации

--- src/pages/register-page.tsx
+++ src/pages/register-page.tsx
@@ -67,7 +67,9 @@ export const RegisterPage = (): React.JSX.Element => {
         </Button>
       </form>
 
-      <p className="page_message text text_type_main-default mt-6" role="alert">\n        {error ?? ''}\n      </p>
+      <p className="page_message text text_type_main-default mt-6" role="alert">
+        {error ?? ''}
+      </p>
 
       <p className="text text_type_main-default text_color_inactive mt-20">
         Уже зарегистрированы?{' '}
